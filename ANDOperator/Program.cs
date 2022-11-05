045b8e2e02a7d4ce92c0ad95584a289f55f79593
aleks257 <afrolov@tktk.ee>
2022-11-05 10:30:33 +0200

    week5

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90; Bioligy >= 90; Chemistry >= 90;
+
+int math, bioligy, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Bioligy result:");
+bioligy = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && bioligy >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
---
+++ OROperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//Math >= 90 OR Chemistry >= 90
+
+//math && chemistry || math && bioligy || chemistry && bioligy
+
+int math, bioligy, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Bioligy result:");
+bioligy = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && bioligy >= 90) || (chemistry >= 90 && bioligy >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
